9372ae72765f8ce196efc9a662a20f3f6cf89d78
Vidhya1416 <nvidhyashreenagaraj@gmail.com>
2025-02-05 22:12:06 +0530

    Update add_users.py

--- add_users.py
+++ add_users.py
@@ -2,26 +2,26 @@ from database import add_user
 import bcrypt
 
 users = [
-    ("john_doe", "password"),
-    ("jane_smith", "password123"),
-    ("alice_johnson", "pass4567!"),
-    ("bob_brown", "myp@ssword88"),
-    ("charlie_davis", "qwertyUI12"),
-    ("david_lee", "secret!2022"),
-    ("emily_clark", "password567"),
-    ("frank_white", "securePass!@#"),
-    ("grace_hall", "letmein123!"),
-    ("hannah_king", "abcdefGHI12"),
-    ("ian_adams", "superSecret90!"),
-    ("jessica_wright", "hunter2#pass"),
-    ("kevin_baker", "myS3curePwd!"),
-    ("laura_anderson", "s3cur1tyTest$"),
-    ("michael_hill", "p@$$w0rd789"),
-    ("natalie_martin", "opensesame#12"),
-    ("oscar_thompson", "TrickyPass@98"),
-    ("paul_garcia", "welcome1@3$"),
-    ("quincy_moore", "simplePass$12"),
-    ("rachel_morris", "loginTest098@")
+    ("john_doe_123", "password!2"),
+    ("jane_smith_456", "password#3"),
+    ("alice_johnson_789", "pass4567!"),
+    ("bob_brown_1011", "myp@ssword88"),
+    ("charlie_davis_1213", "qwertyUI!2"),
+    ("david_lee_1415", "secret!2022"),
+    ("emily_clark_1617", "password$5"),
+    ("frank_white_1819", "securePass!#"),
+    ("grace_hall_2021", "letmein123!"),
+    ("hannah_king_2223", "abcdefGHI!2"),
+    ("ian_adams_2425", "superSecret90!"),
+    ("jessica_wright_2627", "hunter2#pass"),
+    ("kevin_baker_2829", "myS3curePwd!"),
+    ("laura_anderson_3031", "s3cur1tyTest$"),
+    ("michael_hill_3233", "p@$$w0rd789"),
+    ("natalie_martin_3435", "opensesame#12"),
+    ("oscar_thompson_3637", "TrickyPass@98"),
+    ("paul_garcia_3839", "welcome1@3$"),
+    ("quincy_moore_4041", "simplePass$12"),
+    ("rachel_morris_4243", "loginTest098@")
 ]
 
 for username, password in users:
