f0f058f16209989a284d0747a33e2f4fe832883a
Vidhya1416 <nvidhyashreenagaraj@gmail.com>
2025-02-05 08:55:04 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from role_selection import open_role_selection
+
+def main():
+    global root
+    root = tk.Tk()
+    root.title("Tamil Nadu Welfare Connect")
+    root.geometry("600x400")
+
+    open_role_selection(root)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
