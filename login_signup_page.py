23bfb0b2ba56eaedc0a3607c67ba802d3890ef12
Vidhya1416 <nvidhyashreenagaraj@gmail.com>
2025-02-18 22:32:38 +0530

    Update login_signup_page.py

--- login_signup_page.py
+++ login_signup_page.py
@@ -1,4 +1,3 @@
-# login_signup_page.py
 import tkinter as tk
 from utils import set_background
 
@@ -11,11 +10,11 @@ def show_login_signup(role, root, back_callback, show_login_callback, show_signu
     title_label = tk.Label(root, text=f"{role} Login/Signup", font=("Helvetica", 16))
     title_label.pack(pady=20)
 
-    login_button = tk.Button(root, text="Login", command=lambda: show_login_callback(role, root, back_callback))  # Call show_login
+    login_button = tk.Button(root, text="Login", command=lambda: show_login_callback(role))
     login_button.pack(pady=10)
 
     if role.lower() != "admin":  # Check if the role is not admin
-        signup_button = tk.Button(root, text="Signup", command=lambda: show_signup_callback(role, root, back_callback))  # Call show_signup
+        signup_button = tk.Button(root, text="Signup", command=lambda: show_signup_callback(role, root, back_callback))
         signup_button.pack(pady=10)
 
     back_arrow = tk.Label(root, text="←", font=("Helvetica", 24), cursor="hand2", fg="blue", bg="white")
