fadea63ea90b73abf73587897e792fcb5f5b858d
Vidhya1416 <nvidhyashreenagaraj@gmail.com>
2025-02-18 14:09:26 +0530

    Update app_controller.py

--- app_controller.py
+++ app_controller.py
@@ -4,8 +4,7 @@ from role_selection import open_role_selection
 from login import show_login, handle_login, open_dashboard
 from signup import show_signup
 from admin_management import show_admin_management
-from utils import set_background
-from student_dashboard import show_student_dashboard  # Import the student dashboard function
+from student_dashboard import show_student_dashboard
 
 class AppController:
     def __init__(self, root):
@@ -18,7 +17,7 @@ class AppController:
         show_login_signup(role, self.root, self.show_role_selection, self.show_login, self.show_signup)
 
     def show_login(self, role, root, back_callback):
-        show_login(role, self.root, back_callback, self.show_admin_management)
+        show_login(role, self.root, back_callback, self.show_admin_management, self.show_student_dashboard)
 
     def show_signup(self, role, root, back_callback):
         show_signup(role, self.root, back_callback)
@@ -26,5 +25,5 @@ class AppController:
     def show_admin_management(self, username):
         show_admin_management(self.root, lambda: self.show_login("Admin", self.root, self.show_role_selection))
 
-    def show_student_dashboard(self):  # Add this method
-        show_student_dashboard(self.root)
+    def show_student_dashboard(self, root):
+        show_student_dashboard(self.root, back_callback=lambda: self.show_login("Student", self.root, self.show_role_selection))
